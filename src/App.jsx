51e178f7e62a2d6b0b963629c68bb56c135bc84c
Jorge Bello <=>
2022-10-08 23:39:10 -0500

    Comienzo de app y dependencias @emotion/react y @emotion/styled para styled components añadido

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+`
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>Hola Mundo</Heading>
+    </div>
+  )
+}
+
+export default App
