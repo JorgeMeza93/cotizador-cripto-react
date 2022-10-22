4d237567c016a4ca14bdbdba2c51b595b1ffe362
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-21 21:22:12 -0500

    Consultar cotizacion

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,17 @@ const Imagen = styled.img`
 ` 
 function App() {
   const [monedas, setMonedas] = useState({})
+  const [resultado, setResultado] = useState({})
   useEffect( () => {
     if(Object.keys(monedas).length > 0){
-      console.log(monedas)
+      const cotizarCripto = async () => {
+        const {moneda, criptomoneda} = monedas;
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setResultado(resultado.DISPLAY[criptomoneda][moneda]);
+      }
+      cotizarCripto();
     }
   }, [monedas])
 
